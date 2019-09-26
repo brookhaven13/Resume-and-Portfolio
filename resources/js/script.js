6363f319a73a8bcc10582700ea77690c3d8cedf3
brookhaven13 <yu.h.lin1991@gmail.com>
2019-09-26 23:02:25 +0800

    Created the portfolio section
    Created the portfolio section without styling it with CSS

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,5 +61,16 @@ $(document).ready(function () {
         }
 
     });
+    
+    $("[date-fancybox]").fancybox();
+    
+    $(".items").isotope({
+        filter:'*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
 
 });
