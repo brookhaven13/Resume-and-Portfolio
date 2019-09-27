aac6353b711e678910bbae218006fe3ba96b044c
brookhaven13 <yu.h.lin1991@gmail.com>
2019-09-27 18:10:28 +0800

    upload the full portfolio page
    upload the full portfolio page

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,16 +61,5 @@ $(document).ready(function () {
         }
 
     });
-    
-    $("[date-fancybox]").fancybox();
-    
-    $(".items").isotope({
-        filter:'*',
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
-        }
-    });
 
 });
